d25b6effc77b9f34157e8a237dcd62d6654fdfe8
as-f <as-f@users.noreply.github.com>
2018-07-17 09:11:03 -0500

    Update html_root_url

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![deny(warnings, missing_docs, missing_debug_implementations)]
-#![doc(html_root_url = "https://docs.rs/slotmap/0.2.0")]
+#![doc(html_root_url = "https://docs.rs/slotmap/0.3.0")]
 #![crate_name = "slotmap"]
 
 //! # slotmap

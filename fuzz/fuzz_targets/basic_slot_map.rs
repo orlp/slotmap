deab46d73d95a8ab4f1f2c2f9fb9edd2ad0f794d
5225225 <5225225@mailbox.org>
2021-07-02 19:33:58 +0100

    Deduplicate target using macro, add more fuzzed methods

--- fuzz/fuzz_targets/basic_slot_map.rs
+++ fuzz/fuzz_targets/basic_slot_map.rs
@@ -1,39 +1,8 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
-use slotmap::SlotMap;
-
 mod target;
-use target::{Constructor, Op, Target};
 
-fuzz_target!(|data: Target| {
-    let mut map = match data.ctor {
-        Constructor::New => SlotMap::new(),
-        Constructor::WithCapacity(n) => SlotMap::with_capacity(n as usize),
-    };
-
-    let mut keys = Vec::new();
-
-    for op in data.ops {
-        match op {
-            Op::Reserve(n) => map.reserve(n as usize),
-            Op::Insert => keys.push(map.insert(())),
-            Op::InsertWithKey => keys.push(map.insert_with_key(|_| ())),
-            Op::Remove(k) => {
-                if let Some(k) = keys.get(k) {
-                    map.remove(*k);
-                } else {
-                    return;
-                }
-            }
-            Op::Retain(s) => {
-                let mut i = s.into_iter();
-                map.retain(|_k, _v| i.next().unwrap_or(false));
-            }
-            Op::Clear => map.clear(),
-            Op::Drain => {
-                map.drain();
-            }
-        }
-    }
+fuzz_target!(|data: target::Target| {
+    fuzz_slotmap!(data, slotmap::SlotMap::<slotmap::DefaultKey, u32>);
 });

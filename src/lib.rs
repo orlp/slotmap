c9cf6651ac8a5ba862d54f05060d49e263189971
Orson Peters <orsonpeters@gmail.com>
2023-10-31 20:34:50 +0100

    Remove unused lint.

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@
     unused_lifetimes,
     unused_import_braces
 )]
-#![deny(missing_docs, unaligned_references)]
+#![deny(missing_docs)]
 #![cfg_attr(feature = "cargo-clippy", allow(renamed_and_removed_lints))]
 #![cfg_attr(feature = "cargo-clippy", deny(clippy, clippy_pedantic))]
 #![cfg_attr(
